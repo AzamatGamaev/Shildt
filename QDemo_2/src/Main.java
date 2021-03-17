2eb7464d93fa29428f724534c1d856a8363686fc
GamaevAM <azamatgamaev@gmail.com>
2021-03-17 17:59:03 +0300

    Add QDemo.

---
+++ QDemo_2/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Queue q1 = new Queue(10);
+
+        char name[] = {'T', 'o', 'm'};
+
+        Queue q2 = new Queue(name);
+
+        char ch;
+        int i;
+
+        for (i = 0; i < 10; i++)
+            q1.put((char) ('A' + i));
+
+        Queue q3 = new Queue(q1);
+
+        System.out.println("Содержимое q1: ");
+        for (i = 0; i < 10; i++) {
+            ch = q1.get();
+            System.out.print(ch);
+        }
+
+        System.out.println("\n");
+
+        System.out.println("Содержимое q2: ");
+        for (i = 0; i < 3; i++) {
+            ch = q2.get();
+            System.out.print(ch);
+        }
+
+        System.out.println("\n");
+
+        System.out.println("Содержимое q3: ");
+        for (i = 0; i < 10; i++) {
+            ch = q3.get();
+            System.out.print(ch);
+        }
+    }
+}
